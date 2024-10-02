b8d236d4a233c1101edf4cc8600d2cbdffd5995f
SACHIN YADAV <103230520+yaduvanshisachin@users.noreply.github.com>
2024-10-02 20:43:25 +0530

    Add files via upload
    This is Simple Sidebar

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuBtn = document.querySelector('.menu');
+const sidebar = document.querySelector('.sidebar');
+// const menuBtn = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+    sidebar.classList.add('showSidebar');
+});
+
+document.addEventListener('mouseup', (e)=>{
+    if(!sidebar.contains(e.target)){
+        sidebar.classList.remove('showSidebar');
+    }
+})
